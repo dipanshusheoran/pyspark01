1f0a4390f07f7beb7d37a8cb19f3dfc52c743b00
dipanshusheoran <dipanshusheoran@gmail.com>
2022-12-15 21:01:11 +0530

    message

---
+++ spark1.py
@@ -0,0 +1,17 @@
+import sys,os
+from pyspark.sql import *
+os.environ['PYSPARK_PYTHON'] = sys.executable
+os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+spark = SparkSession \
+        .builder \
+        .appName("HelloSpark") \
+        .master("local[2]") \
+        .getOrCreate()
+
+
+columns = ["language","users_count"]
+data = [("Java", "20000"), ("Python", "100000"), ("Scala", "3000")]
+
+df = spark.createDataFrame(data,columns)
+
+df.show()
